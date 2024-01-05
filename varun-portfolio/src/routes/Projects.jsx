606590ed6f0bb50c8752fe5b9c22121f709daa09
Varun Jatav <mohit.mohit979@gmail.com>
2024-01-05 17:43:25 +0530

    new project included

--- varun-portfolio/src/routes/Projects.jsx
+++ varun-portfolio/src/routes/Projects.jsx
@@ -6,11 +6,11 @@ const Projects = () => {
       <h2 className="pb-2 border-bottom">Projects</h2>
 
       <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-        <div className="col">
+        <div className="col" style={{height: "400px"}}>
           <div
             className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
             style={{
-              backgroundImage: "url('/black-pearl.png')",
+              background: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/black-pearl.png')",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center",
               backgroundSize: "cover",
@@ -42,11 +42,11 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="col">
+        <div className="col" style={{height: "400px"}}>
           <div
             className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
             style={{
-              backgroundImage: "url('/weather-app.png')",
+              background: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/weather-app.png')",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center",
               backgroundSize: "cover",
@@ -54,56 +54,57 @@ const Projects = () => {
           >
             <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
               <ul className="d-flex list-unstyled mt-auto">
-                <a href="https://github.com/varunjatav/weather-app"
-                 className="text-decoration-none">
-                <li className="d-flex align-items-center text-white me-3">
-                  <FaGithub />
-                  <small>Github</small>
-                </li>
+                <a
+                  href="https://github.com/varunjatav/weather-app"
+                  className="text-decoration-none"
+                >
+                  <li className="d-flex align-items-center text-white me-3">
+                    <FaGithub />
+                    <small>Github</small>
+                  </li>
                 </a>
-                <a href="https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/"
-                 className="text-decoration-none">
-                <li className="d-flex align-items-center text-white">
-                  <BiLogoNetlify />
-                  <small>Deploy</small>
-                </li>
+                <a
+                  href="https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/"
+                  className="text-decoration-none"
+                >
+                  <li className="d-flex align-items-center text-white">
+                    <BiLogoNetlify />
+                    <small>Deploy</small>
+                  </li>
                 </a>
               </ul>
             </div>
           </div>
         </div>
 
-        <div className="col">
+        <div className="col" style={{height: "400px"}}>
           <div
             className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-            style={{ backgroundImage: "url('unsplash-photo-3.jpg')" }}
+            style={{
+              background: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url('/e-commerce.png')",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              
+            }}
           >
             <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-              <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                Another longer title belongs here
-              </h3>
+              
               <ul className="d-flex list-unstyled mt-auto">
-                <li className="me-auto">
-                  <img
-                    src="https://github.com/twbs.png"
-                    alt="Bootstrap"
-                    width="32"
-                    height="32"
-                    className="rounded-circle border border-white"
-                  />
+                <a href="https://github.com/varunjatav/e-coomerce/tree/main/E-commerce"
+                 className="text-decoration-none">
+                <li className="d-flex align-items-center text-white me-3">
+                  <FaGithub />
+                  <small>Github</small>
                 </li>
-                <li className="d-flex align-items-center me-3">
-                  <svg className="bi me-2" width="1em" height="1em">
-                    <use xlink:href="#geo-fill"></use>
-                  </svg>
-                  <small>California</small>
+                </a>
+                <a href="https://6597ed488b3284009787de86--magnificent-faloodeh-17d228.netlify.app/" 
+                 className="text-decoration-none">
+                <li className="d-flex align-items-center text-white">
+                  <BiLogoNetlify />
+                  <small>Deploy</small>
                 </li>
-                <li className="d-flex align-items-center">
-                  <svg className="bi me-2" width="1em" height="1em">
-                    <use xlink:href="#calendar3"></use>
-                  </svg>
-                  <small>5d</small>
-                </li>
+                </a>
               </ul>
             </div>
           </div>
