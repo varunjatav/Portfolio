e39ce02b1aa3a6a1de4fed3ad7ebffc0c3d86fba
Varun Jatav <mohit.mohit979@gmail.com>
2024-01-09 16:54:33 +0530

    projects

--- varun-portfolio/src/components/Header.jsx
+++ varun-portfolio/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const [active,setActive] = useState("Home");
   return (
 
-      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <header className="d-flex flex-wrap justify-content-center px-3 py-3 mb-4 border-bottom">
         <Link
           to="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none gap-3"
--- varun-portfolio/src/routes/Projects.jsx
+++ varun-portfolio/src/routes/Projects.jsx
@@ -6,78 +6,114 @@ const Projects = () => {
       <h2 className="pb-2 border-bottom">Projects</h2>
 
       <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-        <div className="col" style={{height: "400px"}}>
-          <div
-            className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-            style={{
-              background: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/black-pearl.png')",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-            }}
-          >
-            <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-              {/* <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3> */}
-              <ul className="d-flex list-unstyled mt-auto">
-                <a
-                  href="https://github.com/nikhildeora/black-pearl"
-                  className="text-decoration-none"
-                >
-                  <li className="d-flex align-items-center text-white me-3 gap-2">
-                    <FaGithub />
-                    <small>Github</small>
-                  </li>
-                </a>
-                <a
-                  href="https://startling-tulumba-8a76b2.netlify.app/"
-                  className="text-decoration-none"
-                >
-                  <li className="d-flex align-items-center text-white gap-2">
-                    <BiLogoNetlify />
-                    <small>Deploy</small>
-                  </li>
-                </a>
-              </ul>
+        <div className="col" style={{ height: "500px" }}>
+          <div className="card card-cover h-100 rounded-4 ">
+            <div
+              className=" overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                background:
+                  "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/black-pearl.png')",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                height: "350px",
+              }}
+            >
+              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <ul className="d-flex list-unstyled mt-auto">
+                  <a
+                    href="https://github.com/nikhildeora/black-pearl"
+                    className="text-decoration-none"
+                  >
+                    <li className="d-flex align-items-center text-white me-3 gap-2">
+                      <FaGithub />
+                      <small>Github</small>
+                    </li>
+                  </a>
+                  <a
+                    href="https://startling-tulumba-8a76b2.netlify.app/"
+                    className="text-decoration-none"
+                  >
+                    <li className="d-flex align-items-center text-white gap-2">
+                      <BiLogoNetlify />
+                      <small>Deploy</small>
+                    </li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+            <div className="d-flex flex-wrap align-items-center gap-4  p-3">
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                Javascript
+              </div>
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                HTML 5
+              </div>
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                CSS 3
+              </div>
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                React
+              </div>
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                Firebase-Auth
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col" style={{ height: "500px" }}>
+          <div className="card card-cover h-100 rounded-4 ">
+            <div
+              className="overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                background:
+                  "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/weather-app.png')",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                height: "350px",
+              }}
+            >
+              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <ul className="d-flex list-unstyled mt-auto">
+                  <a
+                    href="https://github.com/varunjatav/weather-app"
+                    className="text-decoration-none"
+                  >
+                    <li className="d-flex align-items-center text-white me-3 gap-2">
+                      <FaGithub />
+                      <small>Github</small>
+                    </li>
+                  </a>
+                  <a
+                    href="https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/"
+                    className="text-decoration-none"
+                  >
+                    <li className="d-flex align-items-center text-white gap-2">
+                      <BiLogoNetlify />
+                      <small>Deploy</small>
+                    </li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+            <div className="d-flex flex-wrap align-items-center gap-4  p-3">
+             
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                HTML 5
+              </div>
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                CSS 3
+              </div>
+              <div className="border border-dark p-1 rounded fw-bold p-2">
+                React JS
+              </div>
+             
             </div>
           </div>
         </div>
 
-        <div className="col" style={{height: "400px"}}>
-          <div
-            className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-            style={{
-              background: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/weather-app.png')",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-            }}
-          >
-            <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-              <ul className="d-flex list-unstyled mt-auto">
-                <a
-                  href="https://github.com/varunjatav/weather-app"
-                  className="text-decoration-none"
-                >
-                  <li className="d-flex align-items-center text-white me-3">
-                    <FaGithub />
-                    <small>Github</small>
-                  </li>
-                </a>
-                <a
-                  href="https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/"
-                  className="text-decoration-none"
-                >
-                  <li className="d-flex align-items-center text-white">
-                    <BiLogoNetlify />
-                    <small>Deploy</small>
-                  </li>
-                </a>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="col" style={{height: "400px"}}>
+        {/* <div className="col" style={{height: "400px"}}>
           <div
             className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
             style={{
@@ -108,7 +144,7 @@ const Projects = () => {
               </ul>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
