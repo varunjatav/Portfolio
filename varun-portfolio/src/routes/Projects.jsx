e9397d873f689a205df6149065a2a90dd1d607a4
Varun Jatav <mohit.mohit979@gmail.com>
2024-01-10 17:14:59 +0530

    project section updated

--- varun-portfolio/src/routes/Projects.jsx
+++ varun-portfolio/src/routes/Projects.jsx
@@ -1,150 +1,92 @@
 import { FaGithub } from "react-icons/fa";
 import { BiLogoNetlify } from "react-icons/bi";
+
+const projects = [
+  {
+    id: 1,
+    img: "/black-pearl.png",
+    title: "Weather app",
+    content:
+      "Black Pearl is an Ecommerce Website where you can find a variety of jewelleries of Gold, Platinum, Solitiare, Diamond, Silver, etc and more",
+    github: "https://github.com/nikhildeora/black-pearl",
+    netlify:
+      "https://startling-tulumba-8a76b2.netlify.app/",
+    techStack: ["/html.png", "/css.png" , "/javascript.png", "/react.png", "/bootstrap.png"],
+  },
+  {
+    id: 2,
+    img: "/weather-app.png",
+    title: "Weather app",
+    content:
+      "A React weather app where you can check weathers of cities and countries",
+    github: "https://github.com/varunjatav/weather-app",
+    netlify:
+      "https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/",
+    techStack: ["/html.png", "/css.png", "/react.png", "/bootstrap.png"],
+  },
+  {
+    id: 3,
+    img: "/weather-app.png",
+    title: "Weather app",
+    content:
+      "A React weather app where you can check weathers of cities and countries",
+    github: "https://github.com/varunjatav/weather-app",
+    netlify:
+      "https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/",
+    techStack: ["/html.png", "/css.png", "/react.png", "/bootstrap.png"],
+  },
+];
 const Projects = () => {
   return (
     <div className="container px-4 py-5" id="custom-cards">
       <h2 className="pb-2 border-bottom">Projects</h2>
 
-      <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-        <div className="col" style={{ height: "500px" }}>
-          <div className="card card-cover h-100 rounded-4 ">
-            <div
-              className=" overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                background:
-                  "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/black-pearl.png')",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                height: "350px",
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <ul className="d-flex list-unstyled mt-auto">
-                  <a
-                    href="https://github.com/nikhildeora/black-pearl"
-                    className="text-decoration-none"
-                  >
-                    <li className="d-flex align-items-center text-white me-3 gap-2">
-                      <FaGithub />
-                      <small>Github</small>
-                    </li>
-                  </a>
-                  <a
-                    href="https://startling-tulumba-8a76b2.netlify.app/"
-                    className="text-decoration-none"
-                  >
-                    <li className="d-flex align-items-center text-white gap-2">
-                      <BiLogoNetlify />
-                      <small>Deploy</small>
-                    </li>
-                  </a>
-                </ul>
+      <div className="row row-cols-1 align-items-stretch g-4 py-5">
+        {projects.map((single) => (
+          <div key={single.id} className="col" style={{ height: "500px" }}>
+            <div className="row row-cols-1 row-cols-lg-2">
+              <div className="col">
+                <img
+                  src={single.img}
+                  alt={single.title}
+                  className="w-100 rounded-s-2"
+                />
               </div>
-            </div>
-            <div className="d-flex flex-wrap align-items-center gap-4  p-3">
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                Javascript
-              </div>
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                HTML 5
-              </div>
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                CSS 3
-              </div>
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                React
-              </div>
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                Firebase-Auth
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col" style={{ height: "500px" }}>
-          <div className="card card-cover h-100 rounded-4 ">
-            <div
-              className="overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                background:
-                  "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url('/weather-app.png')",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                height: "350px",
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <ul className="d-flex list-unstyled mt-auto">
-                  <a
-                    href="https://github.com/varunjatav/weather-app"
-                    className="text-decoration-none"
-                  >
-                    <li className="d-flex align-items-center text-white me-3 gap-2">
-                      <FaGithub />
-                      <small>Github</small>
-                    </li>
-                  </a>
-                  <a
-                    href="https://658ea8986ca12d9c5896be81--incredible-haupia-446af4.netlify.app/"
-                    className="text-decoration-none"
-                  >
-                    <li className="d-flex align-items-center text-white gap-2">
-                      <BiLogoNetlify />
-                      <small>Deploy</small>
-                    </li>
-                  </a>
-                </ul>
-              </div>
-            </div>
-            <div className="d-flex flex-wrap align-items-center gap-4  p-3">
-             
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                HTML 5
-              </div>
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                CSS 3
-              </div>
-              <div className="border border-dark p-1 rounded fw-bold p-2">
-                React JS
-              </div>
-             
-            </div>
-          </div>
-        </div>
+              <div className="col text-center">
+                <h1 className="fs-2">{single.title}</h1>
 
-        {/* <div className="col" style={{height: "400px"}}>
-          <div
-            className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-            style={{
-              background: "linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url('/e-commerce.png')",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              
-            }}
-          >
-            <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-              
-              <ul className="d-flex list-unstyled mt-auto">
-                <a href="https://github.com/varunjatav/e-coomerce/tree/main/E-commerce"
-                 className="text-decoration-none">
-                <li className="d-flex align-items-center text-white me-3">
-                  <FaGithub />
-                  <small>Github</small>
-                </li>
-                </a>
-                <a href="https://6597ed488b3284009787de86--magnificent-faloodeh-17d228.netlify.app/" 
-                 className="text-decoration-none">
-                <li className="d-flex align-items-center text-white">
-                  <BiLogoNetlify />
-                  <small>Deploy</small>
-                </li>
-                </a>
-              </ul>
+                <p className="fst-italic fs-5">{single.content}</p>
+                <div className="d-flex gap-2 justify-content-center">
+                  <a
+                    href={single.github}
+                    type="button"
+                    className="btn btn-dark"
+                  >
+                    <FaGithub /> Github
+                  </a>
+                  <a
+                    href={single.netlify}
+                    type="button"
+                    className="btn btn-primary"
+                  >
+                    <BiLogoNetlify /> Netlify
+                  </a>
+                </div>
+                <div className="d-flex flex-wrap align-items-center justify-content-between pt-4">
+                  {single.techStack.map((tech) => (
+                    <img
+                      src={tech}
+                      alt="javascript"
+                      width="50px"
+                      height="50px"
+                      className="m-auto"
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
-        </div> */}
+        ))}
       </div>
     </div>
   );
